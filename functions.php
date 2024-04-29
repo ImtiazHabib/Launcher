44fc56bce729ce1789fc0ef89f334c48a2a73359
ImtiazHabib <imtiazhabib7@gmail.com>
2024-04-29 17:12:17 +0600

    asset include,code spliting,basic header footer setup

--- functions.php
+++ functions.php
@@ -1,9 +1,27 @@
 <?php
 
 function launcher_basic_setup(){
-	load_textdomain("launcher");
+	load_theme_textdomain("launcher");
 	add_theme_support("post-thumbnails");
 	add_theme_support("title-tags");
 }
 add_action("after_setup_theme","launcher_basic_setup");
+
+// asset include in the theme
+function launcher_asset_include(){
+	// css
+    wp_enqueue_style("launcher_main_style", get_stylesheet_uri());
+    wp_enqueue_style("launcher_animate", get_theme_file_uri( "assets/css/animate.css" ));
+    wp_enqueue_style( "launcher_icomoon", get_theme_file_uri("assets/css/icomoon.css"),null,1.0);
+    wp_enqueue_style( "launcher_bootstarp", get_theme_file_uri( "assets/css/bootstrap.css" ),null,1.0);
+    
+	// js
+	wp_enqueue_script('launher_main_js', get_theme_file_uri( "assets/js/main.js" ), array('jquery'), '1.0', false);
+	wp_enqueue_script('launher_easing', get_theme_file_uri( "assets/js/jquery.easing.1.3.js" ), array('jquery'), '1.0', false);
+	wp_enqueue_script('launher_bootstrap', get_theme_file_uri( "assets/js/bootstrap.min.js" ), array('jquery'), '1.0', false);
+	wp_enqueue_script('launher_waypoints', get_theme_file_uri( "assets/js/jquery.waypoints.min.js" ), array('jquery'), '1.0', false);
+	wp_enqueue_script('launher_countdown', get_theme_file_uri( "assets/js/simplyCountdown.js" ), null, '1.0', false);
+}
+add_action("wp_enqueue_scripts","launcher_asset_include");
+
 ?>
--- launcher.php
+++ launcher.php
@@ -1,56 +1,15 @@
 <?php
 /*
-// 
-// Template Name : Launcher Page Layout 
+* Template Name: Launcher
 */
 ?>
-<!DOCTYPE HTML>
-<!--
-	Launcher by freehtml5.co
-	Twitter: http://twitter.com/fh5co
-	URL: http://freehtml5.co
--->
-<html>
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Launcher &mdash; Free Website Template, Free HTML5 Template by FreeHTML5.co</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
-	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FreeHTML5.co" />
+<?php
+ 
+   get_header( );
 
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+?>
+	<body <?php body_class( ); ?>>
 
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
-
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
-	<body>
 	<div class="fh5co-loader"></div>
 
 	<aside id="fh5co-aside" role="sidebar" class="text-center" style="background-image: url(images/img_bg_1_gradient.jpg);">
@@ -106,20 +65,8 @@
 		</div>
 		
 	</div>
-	
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Count Down -->
-	<script src="js/simplyCountdown.js"></script>
-	<!-- Main -->
-	<script src="js/main.js"></script>
-
-	</body>
-</html>
+<?php
+ 
+   get_footer( );
+?>
 
